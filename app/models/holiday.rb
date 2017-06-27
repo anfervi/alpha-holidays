9c3f10ca3497f665f9572ef5d5256b9de7461db7
anfervi <afernandez@alfatecsistemas.es>
2017-06-27 15:42:30 +0200

    Add relations and tests between entities

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,3 +1,4 @@
 class Holiday < ApplicationRecord
   belongs_to :user
+  has_one :work_schedule
 end
--- spec/models/work_schedule_spec.rb
+++ spec/models/work_schedule_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WorkSchedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "should belongs_to holiday" do
+		expect(WorkSchedule.reflect_on_association(:holiday).macro).to eq :belongs_to
+	end
+
+	it "should has_many public_holidays" do
+		expect(WorkSchedule.reflect_on_association(:public_holidays).macro).to eq :has_many
+	end
 end
