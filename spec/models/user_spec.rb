5eea4d3270c1d9393970e6073fe10b5467ea54bb
anfervi <afernandez@alfatecsistemas.es>
2017-06-27 13:50:04 +0200

    test user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
-require 'rails_helper'
+#require 'rails_helper'
+require "spec_helper"
+#require "user"
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+	before :each do
+		@user = User.new
+		@user.name = 'Angel'
+		@user.lastname = 'Fernández'
+		@user.department = 'Imagen Médica'
+		@user.email = 'angelfervi@gmail.com'
+		@user.password = 'Alfatec2017'
+		@user.role = 0
+	end
+
+	it "should be valid with name, lastname, department, email, role and password" do
+		expect(@user).to be_valid
+	end
+
+	it "should not be valid without a name" do
+		@user.name = nil
+		expect(@user).to_not be_valid
+	end
+
+	it "should not be valid without a lastname" do
+		@user.lastname = nil
+		expect(@user).to_not be_valid
+	end
+
+	it "should not be valid without a department" do
+		@user.department = nil
+		expect(@user).to_not be_valid
+	end
+
+	it "should not be valid without a email" do
+		@user.email = nil
+		expect(@user).to_not be_valid
+	end		
+
+	it "should not be valid without a role" do
+		@user.role = nil
+		expect(@user).to_not be_valid
+	end	
+
+	it "should not be valid without a password" do
+		@user.password = nil
+		expect(@user).to_not be_valid
+	end	
 end
