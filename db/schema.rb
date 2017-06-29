9e5656bc338c970297693a8be1d63121426cfdb0
anfervi <afernandez@alfatecsistemas.es>
2017-06-29 08:37:04 +0200

    Add department field to Holidays

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627094155) do
+ActiveRecord::Schema.define(version: 20170629063402) do
 
   create_table "holidays", force: :cascade do |t|
     t.string "year"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170627094155) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "department"
     t.index ["user_id"], name: "index_holidays_on_user_id"
   end
 
