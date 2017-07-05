babdb02fb57d7dc1940a8c4c1d6432bf15d1cb08
anfervi <afernandez@alfatecsistemas.es>
2017-07-05 22:29:04 +0200

    populate database with admin user using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+country = 'Spain'
+
+admin = User.new
+admin.name = 'alpha'
+admin.lastname = 'holidays'
+admin.email = 'alpha_holidays@aplha.es'
+admin.password  = 'Alpha2017'
+admin.password_confirmation  = 'Alpha2017'
+admin.role = 'admin'
+admin.department = 'Imagen Médica'
+admin.save!
