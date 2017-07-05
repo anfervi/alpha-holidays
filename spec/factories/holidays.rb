ed9e5b431ee4bbfa4d3b93e5ad3b448129e4c914
anfervi <afernandez@alfatecsistemas.es>
2017-07-05 11:42:58 +0200

    Add holidays Factory

---
+++ spec/factories/holidays.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :holiday do
+    year                { Faker::Number.between(1980, 2017) }
+    status              { Faker::Number.between(0, 2) }
+    title               { "Mac #{Faker::StarWars.planet}asky" }
+    description         { Faker::Hipster.sentence(4) }
+    start_day           '2017-08-16'
+    end_date            '2017-09-11'
+    user_id             { Faker::Number.between(0, 10) }
+    department          'Imagen Médica'
+  end
+end
