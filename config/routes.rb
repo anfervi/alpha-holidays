cb4774ad81f205e8696ce0668540b9d56445da4a
anfervi <afernandez@alfatecsistemas.es>
2017-07-07 10:42:30 +0200

    Fixing some mistakes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-	  resources :public_holidays
-	  resources :work_schedules
-	  resources :holidays
-	  resources :users  
+    resources :public_holidays
+    resources :work_schedules
+    resources :holidays
+    resources :users
 
   resources :holidays do
-  	member do
-  		get 'validate'
-  		get 'reject'  
-  	end
+    member do
+      get 'validate'
+      get 'reject'
+    end
   end
 
   resources :admin
-	root to: 'holidays#index'
+  root to: 'holidays#index'
 end
