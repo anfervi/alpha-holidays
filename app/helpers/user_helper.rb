37858cbc6d78bde6ac22ce3b27358180be4c8687
anfervi <afernandez@alfatecsistemas.es>
2017-07-13 10:29:33 +0200

    render avatar in layout using user_helper.rb

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,13 @@
+# app/helpers/user_helper.rb
+module UserHelper
+  def render_avatar
+    content_tag :dropdown do
+      if current_user.present? && current_user.avatar?
+        image_tag(current_user.avatar.url(:thumb))
+      else
+        image_tag("/assets/global/images/avatars/user1.png")
+      end
+    end
+  end
+end
+
