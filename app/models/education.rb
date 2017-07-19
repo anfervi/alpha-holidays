fb4f225c0df2d4a8d453bd0bfe76de9271365b41
anfervi <afernandez@alfatecsistemas.es>
2017-07-19 13:20:09 +0200

    add enums

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,4 @@
 class Education < ApplicationRecord
   belongs_to :curriculum
+  enum type: { course: 0, training: 1, degree: 2 }
 end
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,6 @@
 class Language < ApplicationRecord
   belongs_to :curriculum
+  enum speaking: { basic: 0, intermediate: 1, advanced: 2, bilingual: 3, native: 4 }
+  enum writing: { basic: 0, intermediate: 1, advanced: 2, bilingual: 3, native: 4 }
+  enum reading: { basic: 0, intermediate: 1, advanced: 2, bilingual: 3, native: 4 }
 end
