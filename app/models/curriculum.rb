7e93379495c1422ebf69e5717a241ec96c3056d7
anfervi <afernandez@alfatecsistemas.es>
2017-07-19 13:08:30 +0200

    add curriculum relations

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,3 +1,6 @@
 class Curriculum < ApplicationRecord
   belongs_to :user
+  has_many :educations, dependent: :destroy
+  has_many :work_experiences, dependent: :destroy
+  has_many :languages, dependend: :destroy
 end
